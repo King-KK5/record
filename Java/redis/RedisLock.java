113464e8d84293b4bb9a3ee7ef2d27d075b776a2
King-KK5 <59632014+king-kk5@users.noreply.github.com>
2020-01-17 09:53:08 +0800

    实验提交

--- Java/redis/RedisLock.java
+++ Java/redis/RedisLock.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @program: hss-parent
  * @description: redis分布式锁
@@ -24,10 +26,11 @@ public class RedisLock {
     public boolean lock(String key){
         while (true){
             try {
-                if(doLock(key, RedisConstant.EXPIRE_TIME)){
+                if(doLock(key)){
                     break;
                 }
                 Thread.sleep(500);
+                log.info("未获得锁，休眠500ms");
             } catch (InterruptedException e) {
                 log.error("加锁异常",e);
                 return true;
@@ -40,22 +43,12 @@ public class RedisLock {
     /**
      * redis加锁
      * @param key 锁的键值
-     * @param value 超时时间 锁多久超时
      * @return
      */
-    public boolean doLock(String key,String value){
+    public boolean doLock(String key){
         //如果不存在就设置锁的对象及超时时间
-        if(redisTemplate.opsForValue().setIfAbsent(key,value)){
+        if(redisTemplate.opsForValue().setIfAbsent(key,key,RedisConstant.EXPIRE_TIME, TimeUnit.MILLISECONDS)){
             return true;
-        }
-        String currentLockValue  = redisTemplate.opsForValue().get(key);
-        //如果锁过期
-        if(!StringUtils.isEmpty(currentLockValue) && Long.parseLong(currentLockValue)<System.currentTimeMillis()){
-            //取得之前过期锁的时间并且设置当前锁的超时时间 redis单线程保证该步只有一个线程执行
-            String beforeValue = redisTemplate.opsForValue().getAndSet(key,value);
-            if(!StringUtils.isEmpty(beforeValue) && beforeValue.equals(currentLockValue)){
-                return true;
-            }
         }
         return false;
     }
